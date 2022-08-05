a606fcda1b1710f2d8bfdecb2d0cf8e6812bfc52
Leandro Moreira <leandromoreira.dev@gmail.com>
2022-08-05 12:06:19 -0300

    Efeito no header

---
+++ restaurante/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Restaurante Ko </title>
+    <link rel="stylesheet" href="./css/style.css" />
+</head>
+<body>
+    <header>
+        <button class="abrirMenu"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+</svg></button>
+        <button class="fecharMenu"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+  <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+</svg></button>
+
+    <div class="menu">
+        <ul>
+            <li>Home</li>
+            <li>Clientes</li>
+            <li>Comidas</li>
+            <li>Fazer Pedidos</li>
+        </ul>    
+    </div>    
+</header>
+
+    <script>
+        var abrir = document.querySelector(".abrirMenu");
+abrir.addEventListener("click", () =>{
+    var menu = document.querySelector(".menu");
+    menu.style.marginLeft = "0";
+})        
+
+    </script>
+
+</body>
+</html>
